2ee4a919a0bb08041347d7581d0ad01f3b0db8ab
Härkönen <henrik.harkonen@if.fi>
2017-01-31 12:46:44 +0200

    Added collision testcase.

--- src/test/scala/sylcode/SylCodeTest.scala
+++ src/test/scala/sylcode/SylCodeTest.scala
@@ -3,7 +3,7 @@ package sylcode
 import org.scalatest._
 
 class SylCodeTest extends FlatSpec with Matchers {
-  "SylCode" should "always be decodeable" in {
+  "SylCode" must "always be decodeable" in {
 
     for (i <- -10000 to 70000) {
       val coded = SylCode.bigIntToSyllableString(i)
@@ -12,9 +12,21 @@ class SylCodeTest extends FlatSpec with Matchers {
     }
   }
 
-  "Invalid sylcodes in decode" should "return None" in {
+  "Invalid sylcodes in decode" must "return None" in {
     assert(SylCode.syllableStringToBigInt("ttt").isEmpty)
     assert(SylCode.syllableStringToBigInt("").isEmpty)
+  }
+
+  "Encoded id" must "not have collisions" in {
+    for (i <- 0 to 255) {
+      for ( j <- 0 to 255) {
+        val coded = SylCode.bigIntToSyllableString(i)
+        if (i != j) {
+          val another_coded = SylCode.bigIntToSyllableString(j)
+          assert(coded != another_coded)
+        }
+      }
+    }
   }
 
 }
