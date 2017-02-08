13f384af58dd9a7ae118c70245f47dfd1cd80a96
Henrik Härkönen <heharkon@iki.fi>
2017-02-08 19:29:45 +0200

    Notes. (reverted from commit c23a0d387f7ed374eac1755c3d204a7a31cb74be)

--- src/main/scala/sylcode/SylCode.scala
+++ src/main/scala/sylcode/SylCode.scala
@@ -2,21 +2,12 @@ package sylcode
 
 import scala.util.{Success, Try}
 
-/*
-  SCALA:
-
-  - easy to get started: "java without semicolons"
-  - everything is an object, values, functions, etc..
-  - ADTs are modeled as class/object hierarchies
-
- */
-
 object SylCode {
-  private val letters = 'a' to 'z' // infix notation, equals Char('a').to('z'), can omit dots and brackets
+  private val letters = 'a' to 'z'
   private val vowels = List('a', 'e', 'i', 'o', 'u', 'y')
-  private val double_vowels = "ai" :: vowels.map(c => c.toString + c.toString) // cons operator, map() + lambda
+  private val double_vowels = "ai" :: vowels.map(c => c.toString + c.toString)
   private val consonants = (letters.toSet diff vowels.toSet).toList
-  private val all_vowels = vowels ++ double_vowels // list concatenation
+  private val all_vowels = vowels ++ double_vowels
   private val syllables: List[String] = for (x <- consonants; y <- all_vowels) yield s"$x$y"
 
   def bigIntToSyllableString(input: BigInt): String = {
